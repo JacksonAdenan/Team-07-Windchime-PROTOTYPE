09976f5cc9dcb3d398610335b1b6f282b7c26931
Daniel <pingimingi01@gmail.com>
2020-10-02 23:06:09 +1000

    Worked on PlayerController and selecting items.
    * Got selecting working.
    * Began working on a Pick Up Item UI that appears when selecting an item.

--- Team-07-Windchime-PROTOTYPE/Assets/Scripts/MouseLook.cs
+++ Team-07-Windchime-PROTOTYPE/Assets/Scripts/MouseLook.cs
@@ -5,24 +5,38 @@ using UnityEngine;
 enum CameraMode
 { 
     lookMode,
-    handMode
+    handMode,
+    pauseMode
 }
 public class MouseLook : MonoBehaviour
 {
-
+    // Adjustable Values //
+    // ------------------------------------------ //
     public float mouseSensitivity = 100f;
     public float handControlSensitivity = 100f;
     public float handZDistance = 0.7f;
+    public float PickUpUIPos = 0.0f;
+    // ------------------------------------------ //
+
+    // Inspector Variables //
+    // ------------------------------------------ //
     public Transform playerBody;
     public Material selectedMat;
-
+    public Transform testCube;
     public Transform hand;
+    public Canvas PickUpUI;
+    // ------------------------------------------ //
+
+
     Vector3 handPos;
+    private CameraMode cameraMode = CameraMode.lookMode;
     private Transform selectedObj;
     private Material defaultMat;
     float xRotation = 0.0f;
     RaycastHit raycastHit;
-    private CameraMode cameraMode = CameraMode.lookMode;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -50,38 +64,12 @@ public class MouseLook : MonoBehaviour
 
         else if (cameraMode == CameraMode.handMode)
         {
-            
-            Physics.Raycast(hand.position, hand.forward * 10, out raycastHit);
-            if (raycastHit.transform != null && raycastHit.transform.CompareTag("Item"))
-            {
-                selectedObj = raycastHit.transform;
-                defaultMat = selectedObj.GetComponent<Renderer>().material;
-
-            }
-            else
-            {
-                selectedObj = null;
-            }
-            if (selectedObj != null)
-            {
-                selectedObj.GetComponent<Renderer>().material = selectedMat;
-            }
-            else
-            {
-                selectedObj.GetComponent<Renderer>().material = defaultMat;
-            }
-
             float mouseX = Input.GetAxis("Mouse X") * handControlSensitivity * Time.deltaTime;
             float mouseY = Input.GetAxis("Mouse Y") * handControlSensitivity * Time.deltaTime;
 
-            
-            
             Vector3 handMovement = transform.right * mouseX + transform.up * mouseY;
 
 
-
-            //handMovement.x = Mathf.Clamp(handMovement.x, -0.5f, 0.5f);
-            //handMovement.y = Mathf.Clamp(handMovement.y, -0.5f, 0.5f);
             hand.transform.position += handMovement * Time.deltaTime;
 
             handPos = hand.transform.localPosition;
@@ -89,14 +77,26 @@ public class MouseLook : MonoBehaviour
             handPos.y = Mathf.Clamp(handPos.y, -0.3f, 0.3f);
             handPos.x = Mathf.Clamp(handPos.x, -0.5f, 0.5f);
             hand.transform.localPosition = handPos;
-
+        }
+        else if (cameraMode == CameraMode.pauseMode)
+        { 
             
-            
-
-
         }
 
-        
+        SelectObj();
+
+        if (selectedObj)
+        {
+            PickUpUI.gameObject.SetActive(true);
+            PickUpUI.transform.LookAt(gameObject.transform);
+            Vector3 UIPos = selectedObj.position;
+            UIPos.y += PickUpUIPos;
+            PickUpUI.transform.position = UIPos;
+        }
+        else
+        {
+            PickUpUI.gameObject.SetActive(false);
+        }
 
     }
 
@@ -106,9 +106,36 @@ public class MouseLook : MonoBehaviour
         {
             cameraMode = CameraMode.handMode;
         }
-        else
+        else if (Input.GetMouseButton(2))
         {
             cameraMode = CameraMode.lookMode;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            cameraMode = CameraMode.pauseMode;
+        }
+    }
+
+    void SelectObj()
+    {
+        Physics.Raycast(hand.position, hand.forward * 10, out raycastHit);
+        if (raycastHit.transform != null && raycastHit.transform.CompareTag("Item") && selectedObj != raycastHit.transform)
+        {
+            selectedObj = raycastHit.transform;
+            defaultMat = selectedObj.GetComponent<Renderer>().material;
+            selectedObj.GetComponent<Renderer>().material = selectedMat;
+            Debug.Log("Looking at obj...");
+
+            testCube.gameObject.GetComponent<Renderer>().material = defaultMat;
+        }
+        else if (raycastHit.transform != selectedObj)
+        {
+            Debug.Log("Not looking at obj...");
+            if (selectedObj != null)
+            {
+                selectedObj.GetComponent<Renderer>().material = defaultMat;
+            }
+            selectedObj = null;
         }
     }
 }
