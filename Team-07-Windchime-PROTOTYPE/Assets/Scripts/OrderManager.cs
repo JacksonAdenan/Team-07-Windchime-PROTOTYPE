727057e0ada9af484fc9b67ecabcffcbf1889de3
Daniel <pingimingi01@gmail.com>
2020-10-03 17:18:20 +1000

    * Re-organised folders and added stub classes.

---
+++ Team-07-Windchime-PROTOTYPE/Assets/Scripts/OrderManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderManager : MonoBehaviour
+{
+    private List<Soup> allSoups = new List<Soup>();
+
+    void ReadAllSoups(GameObject soupContainerObject)
+    {
+        for (int i = 0; i < soupContainerObject.transform.childCount; i++)
+        { 
+            //allSoups.Add(soupContainerObject.transform.GetChild()
+        }
+    }
+}
